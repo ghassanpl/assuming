8b2a777c5f78a363968171142474b09d2b5e6cde
kronikarz <kronikarz@gmail.com>
2020-01-10 19:38:58 +0100

    * first commit

---
+++ include/assuming.h
@@ -0,0 +1,96 @@
+/// Copyright 2017-2020 Ghassan.pl
+/// Usage of the works is permitted provided that this instrument is retained with
+/// the works, so that any entity that uses the works is notified of this instrument.
+/// DISCLAIMER: THE WORKS ARE WITHOUT WARRANTY.
+#pragma once
+
+#include <fmt/format.h>
+#include <gsl/gsl_assert>
+#include <gsl/span>
+
+#ifndef ASSUMING_DEBUG
+#ifdef NDEBUG
+#define ASSUMING_DEBUG 0
+#else
+#define ASSUMING_DEBUG 1
+#endif
+#endif
+
+#ifdef _MSC_VER
+#define __PRETTY_FUNCTION__ __FUNCSIG__
+#endif
+
+#define ASSUMING_SL {__LINE__, __FILE__, __PRETTY_FUNCTION__}
+
+#if ASSUMING_DEBUG
+
+#define Assuming(exp, ...) { if (decltype(auto) _assuming_exp_v = (exp); GSL_UNLIKELY(!_assuming_exp_v)) \
+	ReportAssumptionFailure(ASSUMING_SL, #exp " will evalute to true", { { #exp, fmt::format("{}", _assuming_exp_v) } }, detail::AdditionalDataToString(__VA_ARGS__)); }
+
+#define AssumingNull(exp, ...) { if (decltype(auto) _assuming_exp_v = (exp); GSL_UNLIKELY(_assuming_exp_v != nullptr)) \
+	ReportAssumptionFailure(ASSUMING_SL, #exp " will be null", { { #exp, fmt::format("{}", fmt::ptr(_assuming_exp_v)) } }, detail::AdditionalDataToString(__VA_ARGS__)); }
+
+#define AssumingNotNull(exp, ...) { if (decltype(auto) _assuming_exp_v = (exp); GSL_UNLIKELY(_assuming_exp_v == nullptr)) \
+	ReportAssumptionFailure(ASSUMING_SL, #exp " will not be null", { { #exp, fmt::format("{}", fmt::ptr(_assuming_exp_v)) } }, detail::AdditionalDataToString(__VA_ARGS__)); }
+
+#define AssumingBinOp(a, b, op, text, ...) { decltype(auto) _assuming_a_v = (a); decltype(auto) _assuming_b_v = (b); if (GSL_UNLIKELY(!(_assuming_a_v op _assuming_b_v))) \
+	ReportAssumptionFailure(ASSUMING_SL, #a " will " text " " #b, { { #a, fmt::format("{}", _assuming_a_v) }, { #b, fmt::format("{}", _assuming_b_v) } }, detail::AdditionalDataToString(__VA_ARGS__)); }
+
+#define AssumingEqual(a, b, ...) AssumingBinOp(a, b, ==, "be equal to", __VA_ARGS__)
+#define AssumingNotEqual(a, b, ...) AssumingBinOp(a, b, !=, "not be equal to", __VA_ARGS__)
+#define AssumingGreater(a, b, ...) AssumingBinOp(a, b, >, "be greater than", __VA_ARGS__)
+#define AssumingLess(a, b, ...) AssumingBinOp(a, b, <, "be less than", __VA_ARGS__)
+#define AssumingGreaterEqual(a, b, ...) AssumingBinOp(a, b, >=, "be greater or equal to", __VA_ARGS__)
+#define AssumingLessEqual(a, b, ...) AssumingBinOp(a, b, <=, "be less or equal to", __VA_ARGS__)
+
+#define AssumingEmpty(exp, ...) { using std::empty; using std::size; if (decltype(auto) _assuming_exp_v = (exp); GSL_UNLIKELY(!empty(_assuming_exp_v))) \
+	ReportAssumptionFailure(ASSUMING_SL, #exp " will be empty", { { "size of " #exp, fmt::format("{}", size(_assuming_exp_v)) } }, detail::AdditionalDataToString(__VA_ARGS__)); }
+
+#define AssumingNotEmpty(exp, ...) { using std::empty; using std::size; if (decltype(auto) _assuming_exp_v = (exp); GSL_UNLIKELY(empty(_assuming_exp_v))) \
+	ReportAssumptionFailure(ASSUMING_SL, #exp " will not be empty", { { "size of " #exp, fmt::format("{}", size(_assuming_exp_v)) } }, detail::AdditionalDataToString(__VA_ARGS__)); }
+
+#define AssumingNullOrEmpty(exp, ...) { using std::empty; using std::size; if (decltype(auto) _assuming_exp_v = (exp); GSL_UNLIKELY(!detail::IsNullOrEmpty(_assuming_exp_v))) \
+	ReportAssumptionFailure(ASSUMING_SL, #exp " will be empty", { { #exp, _assuming_exp_v ? fmt::format("'{}'", _assuming_exp_v) : "(null)" } }, detail::AdditionalDataToString(__VA_ARGS__)); }
+
+#define AssumingNotNullOrEmpty(exp, ...) { using std::empty; using std::size; if (decltype(auto) _assuming_exp_v = (exp); GSL_UNLIKELY(detail::IsNullOrEmpty(_assuming_exp_v))) \
+	ReportAssumptionFailure(ASSUMING_SL, #exp " will not be empty", { { #exp, _assuming_exp_v ? fmt::format("'{}'", _assuming_exp_v) : "(null)" } }, detail::AdditionalDataToString(__VA_ARGS__)); }
+
+#define AssumingValidIndex(_index, _container, ...) { using std::size; decltype(auto) _assuming_index = (_index); decltype(auto) _assuming_container = (_container); const auto _assuming_container_size = size(_assuming_container); \
+	if (GSL_UNLIKELY(!(_assuming_index >= 0 && size_t(_assuming_index) < _assuming_container_size))) { \
+		ReportAssumptionFailure(ASSUMING_SL, #_index " will be a valid index to " #_container, { \
+			{ #_index, fmt::format("{}", _assuming_index) }, \
+			{  "size of " #_container, fmt::format("{}", _assuming_container_size) }, \
+		}, detail::AdditionalDataToString(__VA_ARGS__)); } }
+
+#else
+
+#define Assuming(exp, ...) { decltype(auto) exp_v = (exp); !!(!exp_v); GSL_ASSUME(!(exp)); }
+
+#endif
+
+namespace detail
+{
+	inline bool IsNullOrEmpty(const char* str) { return str == nullptr || str[0] == 0; }
+	template <typename T>
+	inline bool IsNullOrEmpty(T&& str) { using std::empty; return empty(str); }
+
+	inline std::string AdditionalDataToString() { return {}; }
+	template <typename T, typename... ARGS>
+	inline std::string AdditionalDataToString(T&& fmt, ARGS&&... args) {
+		return fmt::format(std::forward<T>(fmt), std::forward<ARGS>(args)...);
+	}
+
+	struct source_location {
+		long line;
+		std::string_view file_name;
+		std::string_view function_name;
+	};
+}
+
+void ReportAssumptionFailure(detail::source_location where, std::string_view expectation, std::initializer_list<std::pair<std::string_view, std::string>> values, std::string data);
+
+/*
+#define AssumingBetween(v, a, b, ...) { Assuming_GET(v_v, v); Assuming_GET(a_v, a); Assuming_GET(b_v, b); \
+	Assuming_IFFALSE(v_v >= a_v && v_v < b_v) { Assuming_START(); Assuming_FAILED(#v " will be between " #a " (inclusive) and " #b " (exclusive"); \
+	Assuming_VAL(v_v, #v); Assuming_VAL(a_v, #a); Assuming_VAL(b_v, #b); Assuming_FINISH(__VA_ARGS__); } }
+*/
---
+++ test/tests.cpp
@@ -0,0 +1,157 @@
+/// Copyright 2017-2020 Ghassan.pl
+/// Usage of the works is permitted provided that this instrument is retained with
+/// the works, so that any entity that uses the works is notified of this instrument.
+/// DISCLAIMER: THE WORKS ARE WITHOUT WARRANTY.
+
+#define FMT_USE_WINDOWS_H 0
+#define FMT_HEADER_ONLY 1
+#include "../include/assuming.h"
+#include <gtest/gtest.h>
+
+struct AssumingTest;
+AssumingTest* current_test = nullptr;
+
+using name_value_pair = std::pair<std::string_view, std::string>;
+
+struct AssumingTest : public ::testing::Test
+{
+  bool assumption_failed = false;
+  detail::source_location last_where;
+  std::string last_expectation;
+  std::vector<name_value_pair> last_values;
+  std::string last_data;
+
+  size_t evaluation_count = 0;
+
+  template <typename T>
+  auto single_eval_check(T&& v)
+  {
+    evaluation_count++;
+    return std::forward<T>(v);
+  }
+
+  void ReportAssumptionFailure(detail::source_location where, std::string_view expectation, std::initializer_list<name_value_pair> values, std::string data)
+  {
+    assumption_failed = true;
+    last_where = where;
+    last_expectation = expectation;
+    fmt::print("Failed assumption that {}\n", expectation);
+    last_values = { values.begin(), values.end() };
+    last_data = std::move(data);
+  }
+
+  template <typename T, typename U>
+  static inline bool compare(T const& a, U const& b)
+  {
+    return std::equal(std::begin(a), std::end(a), std::begin(b), std::end(b));
+  }
+
+  void SetUp() override
+  {
+    current_test = this;
+  }
+
+  void TearDown() override
+  {
+    current_test = nullptr;
+  }
+};
+
+#define EXPECT_ASSUMPTION_FAILED(func_name, ...) func_name(__VA_ARGS__, "test({}, {})", 0, 5); EXPECT_TRUE(assumption_failed) << #func_name; assumption_failed = false; \
+  EXPECT_EQ(last_where.line, __LINE__); EXPECT_EQ(last_where.file_name, __FILE__); EXPECT_EQ(last_where.function_name, __PRETTY_FUNCTION__); EXPECT_EQ("test(0, 5)", last_data);
+#define EXPECT_ASSUMPTION_SUCCEEDED(func_name, ...) func_name(__VA_ARGS__); EXPECT_FALSE(assumption_failed) << #func_name; assumption_failed = false;
+
+TEST_F(AssumingTest, Assuming_works)
+{
+  EXPECT_ASSUMPTION_SUCCEEDED(Assuming, true);
+
+  const bool value = false;
+  EXPECT_ASSUMPTION_FAILED(Assuming, value);
+
+  name_value_pair const values[] = { {"value", "false"} };
+  ASSERT_TRUE(compare(values, last_values));
+}
+
+TEST_F(AssumingTest, AssumingNotNull_works)
+{
+  EXPECT_ASSUMPTION_SUCCEEDED(AssumingNotNull, &::ReportAssumptionFailure);
+
+  const decltype(&::ReportAssumptionFailure) value = nullptr;
+  EXPECT_ASSUMPTION_FAILED(AssumingNotNull, value);
+
+  name_value_pair const values[] = { {"value", "0x0"} };
+  ASSERT_TRUE(compare(values, last_values));
+}
+
+TEST_F(AssumingTest, AssumingNull_works)
+{
+  const decltype(&::ReportAssumptionFailure) value = nullptr;
+  EXPECT_ASSUMPTION_SUCCEEDED(AssumingNull, value);
+
+  EXPECT_ASSUMPTION_FAILED(AssumingNull, &::ReportAssumptionFailure);
+
+  name_value_pair const values[] = { {"&::ReportAssumptionFailure", fmt::format("{}", fmt::ptr(&::ReportAssumptionFailure)) } };
+  ASSERT_TRUE(compare(values, last_values));
+}
+
+TEST_F(AssumingTest, AssumingEqual_works)
+{
+  std::pair<int, double> q = { 5, 6 };
+  EXPECT_ASSUMPTION_FAILED(AssumingEqual, q.first, q.second);
+
+  name_value_pair const values[] = { {"q.first", "5"}, {"q.second", "6.0"} };
+  ASSERT_TRUE(compare(values, last_values));
+
+  const double value = 0.4;
+  EXPECT_ASSUMPTION_SUCCEEDED(AssumingEqual, value, 0.4);
+}
+
+TEST_F(AssumingTest, AssumingNotEqual_works)
+{
+  EXPECT_ASSUMPTION_SUCCEEDED(AssumingNotEqual, 5, 6);
+
+  const double value = 0.4;
+  EXPECT_ASSUMPTION_FAILED(AssumingNotEqual, value, 0.4);
+
+  name_value_pair const values[] = { {"value", "0.4"}, {"0.4", "0.4"} };
+  ASSERT_TRUE(compare(values, last_values));
+}
+
+#define EXPECT_EVAL_COUNT(count, ...) __VA_ARGS__; EXPECT_EQ(std::exchange(evaluation_count, 0), count);
+TEST_F(AssumingTest, assumings_evaluate_arguments_only_once)
+{
+  EXPECT_EVAL_COUNT(1, Assuming(single_eval_check(false)));
+
+  EXPECT_EVAL_COUNT(1, AssumingNotNull(single_eval_check(decltype(&::ReportAssumptionFailure)(nullptr))));
+  EXPECT_EVAL_COUNT(1, AssumingNull(single_eval_check(&::ReportAssumptionFailure)));
+
+  EXPECT_EVAL_COUNT(2, AssumingEqual(single_eval_check(1), single_eval_check(2)));
+  EXPECT_EVAL_COUNT(2, AssumingNotEqual(single_eval_check(1), single_eval_check(1)));
+  EXPECT_EVAL_COUNT(2, AssumingGreater(single_eval_check(1), single_eval_check(1)));
+  EXPECT_EVAL_COUNT(2, AssumingLess(single_eval_check(1), single_eval_check(1)));
+  EXPECT_EVAL_COUNT(2, AssumingGreaterEqual(single_eval_check(1), single_eval_check(2)));
+  EXPECT_EVAL_COUNT(2, AssumingLessEqual(single_eval_check(1), single_eval_check(0)));
+
+  EXPECT_EVAL_COUNT(1, AssumingEmpty(single_eval_check(std::string{ "hello" })));
+  EXPECT_EVAL_COUNT(1, AssumingNotEmpty(single_eval_check(std::string{})));
+
+  EXPECT_EVAL_COUNT(1, AssumingNullOrEmpty(single_eval_check("hello")));
+  const char* null_string = nullptr;
+  std::string empty_string{};
+  EXPECT_EVAL_COUNT(1, AssumingNotNullOrEmpty(single_eval_check(null_string)));
+
+  EXPECT_EVAL_COUNT(2, AssumingValidIndex(single_eval_check(1), single_eval_check(empty_string)));
+}
+
+/// TODO: Make sure everything works when ASSUMING_DEBUG is not defined
+
+void ReportAssumptionFailure(detail::source_location where, std::string_view expectation, std::initializer_list<name_value_pair> values, std::string data)
+{
+  current_test->ReportAssumptionFailure(where, expectation, std::move(values), std::move(data));
+}
+int main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
